20c841f4f5277fe07b9e073f17a8e10daec6a7f5
OSIELJ <osieljuniormartins@gmail.com>
2024-10-01 08:03:38 -0300

    Configurações iniciais.

---
+++ src/main/java/com/osiel/ApiT/ApiTApplication.java
@@ -0,0 +1,13 @@
+package com.osiel.ApiT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiel/ApiT/ApiTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiel.ApiT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
